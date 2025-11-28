250153cbb267f607957f4cafd8cc4bad848544f7
annafag <fernandes.oliveira.anna@escola.pr.gov.br>
2025-11-28 19:14:24 -0300

    Add comment block and 'comprar' function
    Added a comment block and a 'comprar' function that alerts a message.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+/*  
+    Projeto: Beauty Makeup  
+    Autora: Anna Karolyna – 2B  
+    Funções simples de interação para o site  
+*/
+
+function comprar() {
+    alert("Obrigada por seu interesse! 💖\nEste recurso ainda está em desenvolvimento.");
+}
 
